fa43d52f0f2615cd6c976ef6c6655cc2316b9fea
Mateus <mateustgalvao@outlook.com>
2023-04-29 16:31:54 -0300

    adds the recipes names in each section of /meals

--- src/pages/Meals.tsx
+++ src/pages/Meals.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useContext, useEffect, useState } from 'react'
 import { useFetch } from '../hooks/useFetch'
 import { LoadingContext } from '../context/LoadingContext';
+import RecipeCarrousel from '../components/RecipeCarrousel';
 
 interface ICategory {
   idCategory: string,
@@ -12,16 +13,15 @@ interface ICategory {
 export default function Meals(): JSX.Element {
   const [isReady, setIsReady] = useState<boolean>(false);
   const [categories, setCategories] = useState<ICategory[]>([]);
-  
+
   useEffect(() => {
-    console.log('useEffect')
     const fetchCategories = async () => {
       setIsReady(false);
       const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
       const { categories } = await response.json();
       setCategories(categories);
       setIsReady(true);
-    }
+    };
 
     fetchCategories();
   }, [])
@@ -34,11 +34,13 @@ export default function Meals(): JSX.Element {
             <div>
               <p>{category.strCategory}</p>
               {
-                // <RecipeCarrousel />
+                <RecipeCarrousel
+                  recipeCategory={category.strCategory}
+                />
               }
             </div>
           ))
-        ) : (<h3>Loading ...</h3>)
+        ) : (<h3>Loading...</h3>)
       }
     </div>
   )
